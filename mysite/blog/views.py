b69b476a39f91090026d584d0fdb245022e24899
youker <youkerbz@gmail.com>
2018-04-06 10:17:45 +0800

    使用Django模板系统

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -5,4 +5,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse('欢迎访问我的博客首页~')
+    # return HttpResponse('欢迎访问我的博客首页~')
+    return render(request,'blog/index.html', context={
+        'title': '我的博客首页',
+        'welcome': '欢迎问我的博客首页！！！'
+    })
