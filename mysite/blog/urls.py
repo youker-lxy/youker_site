2275bde8b31e17ae8eb325fb65bd09421f6b51ad
youker <youkerbz@gmail.com>
2018-04-06 09:57:49 +0800

    修改ulrs配置错误

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,10 +8,9 @@
 # @Content : 
 
 
-from django.conf.urls import url
-
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index(), name='index'),
+    path(r'', views.index, name='index'),
 ]
