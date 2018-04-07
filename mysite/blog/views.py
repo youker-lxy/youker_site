e6765aa1a5254849c6ac4dbcbc44a357d558a3e1
youker <youkerbz@gmail.com>
2018-04-07 10:04:39 +0800

    正式博客首页视图test_1

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,12 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Article
 # Create your views here.
 
 
 def index(request):
     # return HttpResponse('欢迎访问我的博客首页~')
-    return render(request,'blog/index.html', context={
-        'title': '我的博客首页',
-        'welcome': '欢迎问我的博客首页！！！'
-    })
+    # return render(request,'blog/index.html', context={
+    #     'title': '我的博客首页',
+    #     'welcome': '欢迎问我的博客首页！！！'
+    # })
+
+    article_list = Article.objects.all().order_by('-created_time')
+    return render(request, 'blog/index.html', context={'article_list': article_list})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+
+
 STATIC_URL = '/static/'
 # 收集静态资源
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
