e206e905527446a8b5243fbf7cebc923a5440224
youker <youkerbz@gmail.com>
2018-04-17 11:39:41 +0800

    markdown参数变动，没有extensions默认参数

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -20,9 +20,12 @@ def index(request):
 def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     # 文章内容使用markdown格式，顶部引入Markdown模块
-    article.body = markdown.markdown(article.body, extensions=[
-                                     'markdown.extensions.extra',
-                                     'markdown.extensions.codehilite',
-                                     'markdown.extensions.toc',
-                                  ])
+    # article.body = markdown.markdown(article.body, extensions=[
+    #                                  'markdown.extensions.extra',
+    #                                  'markdown.extensions.codehilite',
+    #                                  'markdown.extensions.toc',
+    #                               ])
+
+    # markdown参数变动，没有extensions默认参数
+    article.body = markdown.markdown(article.body, ['extra', 'codehilite', 'toc', ])
     return render(request, 'blog/detail.html', context={'article': article})
