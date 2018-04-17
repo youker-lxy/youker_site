087a62314dc75c8043cc987c5ea6de2697988291
youker <youkerbz@gmail.com>
2018-04-17 11:18:33 +0800

    markdown格式应用，语法高亮

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Article
+
+import markdown
+
 # Create your views here.
 
 
@@ -10,11 +13,16 @@ def index(request):
     #     'title': '我的博客首页',
     #     'welcome': '欢迎问我的博客首页！！！'
     # })
-
     article_list = Article.objects.all().order_by('-created_time')
     return render(request, 'blog/index.html', context={'article_list': article_list})
 
 
 def detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
+    # 文章内容使用markdown格式，顶部引入Markdown模块
+    article.body = markdown.markdown(article.body, extensions=[
+                                     'markdown.extensions.extra',
+                                     'markdown.extensions.codehilite',
+                                     'markdown.extensions.toc',
+                                  ])
     return render(request, 'blog/detail.html', context={'article': article})
