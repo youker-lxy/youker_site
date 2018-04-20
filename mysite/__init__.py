b6a77ec2ed1fb2ebc370185ad6383fc7ba484dff
youker <youkerbz@gmail.com>
2018-04-20 12:40:38 +0800

    尝试修改无法在blog中导入表单类问题10

---
+++ mysite/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @Time    : 2018/04/20 12:40
+# @Author  : youker
+# @File    : __init__.py
+# @Contact : youkerbz@gmail.com
+# @Content : 
+
+
+if __name__ == '__main__':
+    pass
